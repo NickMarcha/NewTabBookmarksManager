927689e37d8e63923134a08d571f230ed173fe04
NickMarcha <nicolasmmjoes@gmail.com>
2024-06-23 14:38:10 +0200

    Initial Commit POC

---
+++ src/App.tsx
@@ -0,0 +1,153 @@
+import React, {useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const [newBookmarkTitle, setNewBookmarkTitle] = React.useState('');
+    const [newBookmarkUrl, setNewBookmarkUrl] = React.useState('');
+
+    const [bookmarkItems, setBookmarkItems] = React.useState<browser.bookmarks.BookmarkTreeNode[]>([]);
+
+    useEffect(() => {
+        fetchBookmarks();
+    }, []);
+
+    function fetchBookmarks() {
+        browser.bookmarks.getTree().then(bookmarkItems => {
+            setBookmarkItems(bookmarkItems);
+        });
+    }
+
+    function addBookmarkButtonHandler() {
+
+        if (newBookmarkTitle && newBookmarkUrl) {
+            browser.bookmarks.create({
+                title: newBookmarkTitle,
+                url: newBookmarkUrl
+            }).then(fetchBookmarks);
+        }
+    }
+    console.log(bookmarkItems);
+
+
+    return (
+        <div className="App">
+            {/*<header className="App-header">*/}
+            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+            {/*    <p>*/}
+            {/*        Edit <code>src/App.tsx</code> and save to reload.*/}
+            {/*    </p>*/}
+            {/*    <a*/}
+            {/*        className="App-link"*/}
+            {/*        href="https://reactjs.org"*/}
+            {/*        target="_blank"*/}
+            {/*        rel="noopener noreferrer"*/}
+            {/*    >*/}
+            {/*        Learn React*/}
+            {/*    </a>*/}
+            {/*</header>*/}
+
+            <div>
+                <div id="bookmarks">
+                    {bookmarkItems.map(bookmarkItem => {
+                        return <TreeNode bookmarkTreeNode={bookmarkItem}/>
+                    })}
+
+
+                </div>
+
+
+                <input type="text" id="bookmarkTitle" placeholder="Title"
+                       value={newBookmarkTitle}
+                       onChange={event => setNewBookmarkTitle(event.target.value)}
+                />
+                <input type="url" id="bookmarkUrl" placeholder="URL"
+                       value={newBookmarkUrl}
+                       onChange={event => setNewBookmarkUrl(event.target.value)}
+                />
+                <button id="addBookmark"
+                        onClick={addBookmarkButtonHandler}
+                >Add Bookmark
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function TreeNode({bookmarkTreeNode}: { bookmarkTreeNode: browser.bookmarks.BookmarkTreeNode }) {
+    function RenderType() {
+        switch (bookmarkTreeNode.type) {
+            case 'folder':
+                return <BookmarkFolder bookmarkTreeNode={bookmarkTreeNode}/>
+            case 'bookmark':
+                return <BookmarkElement bookmark={bookmarkTreeNode}/>
+            case 'separator':
+                return <>Separator</>
+            default:
+                return <>Not Supported</>
+        }
+    }
+
+    return <div className={"tree-node"}>
+        <RenderType/>
+        {/*<DebugInfo bookmark={bookmarkTreeNode}/>*/}
+    </div>
+}
+
+
+function BookmarkFolder({bookmarkTreeNode}: { bookmarkTreeNode: browser.bookmarks.BookmarkTreeNode }) {
+    if (bookmarkTreeNode.type !== 'folder' || !bookmarkTreeNode.children) {
+        throw new Error('BookmarkFolder component can only render folder type');
+    }
+
+    return <div className={"bookmark-folder"}>
+        <strong>{bookmarkTreeNode.title}</strong>
+
+        <div>
+            {bookmarkTreeNode.children.map(bookmark => {
+                return <TreeNode bookmarkTreeNode={bookmark}/>
+            })}
+        </div>
+    </div>
+}
+
+function BookmarkElement({bookmark}: { bookmark: browser.bookmarks.BookmarkTreeNode }) {
+
+    if (bookmark.type !== 'bookmark') {
+        throw new Error('BookmarkElement component can only render bookmark type');
+    }
+
+    async function RemoveBookmarkButtonHandler() {
+        await browser.bookmarks.remove(bookmark.id);
+    }
+
+
+    return <>
+        <span>{bookmark.title}</span>
+        <div>
+            <a href={bookmark.url} target="_blank" rel={"noreferrer"}>Visit</a>
+            <button data-id="bookmark.id" onClick={RemoveBookmarkButtonHandler}>Remove</button>
+        </div>
+    </>
+}
+
+function DebugInfo({bookmark}: { bookmark: browser.bookmarks.BookmarkTreeNode }) {
+
+    const [showDebugInfo, setShowDebugInfo] = React.useState(false);
+
+    return <>
+        <button onClick={() => setShowDebugInfo(!showDebugInfo)}>Toggle Debug Info</button>
+        {showDebugInfo && <>
+            <span>id: {bookmark.id}</span><br/>
+            <span>type:  {bookmark.type}</span><br/>
+            <span>url: {bookmark.url}</span><br/>
+            <span>title: {bookmark.title}</span><br/>
+            <span>dateAdded: {bookmark.dateAdded}</span><br/>
+            <span>dateGroupModified: {bookmark.dateGroupModified}</span><br/>
+            <span>index: {bookmark.index}</span><br/>
+            <span>parentId: {bookmark.parentId}</span><br/>
+            <span>unmodifiable: {bookmark.unmodifiable}</span></>}
+    </>
+}
+
+export default App;
